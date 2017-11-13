b99acc8e571bdd6026e238f7b98a788d7f895e1a
arizona2014 <andy.lisac@gmail.com>
2017-11-13 07:36:54 +0100

    rightfull database

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var messageRoutes = require('./routes/messages');
 var userRoutes = require('./routes/user');
 
 var app = express();
-mongoose.connect('localhost:27017/node-angular');
+mongoose.connect('localhost:27017/mean');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
