ad6e63ca2f270951ac1ecfd60849337500e15d07
arizona2014 <andy.lisac@gmail.com>
2017-11-30 08:22:55 +0100

    introducing video

--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -5,7 +5,7 @@ import {Component} from "@angular/core";
         <header class="row">
             <nav class="col-md-8 col-md-offset-2">
                 <ul class="nav nav-pills">
-                    <li routerLinkActive="active"><a [routerLink]="['/messages']">Messenger</a></li>
+                    <li routerLinkActive="active"><a [routerLink]="['/videos']">Videos</a></li>
                     <li routerLinkActive="active"><a [routerLink]="['/auth']">Authentication</a></li>
                 </ul>    
             </nav>
