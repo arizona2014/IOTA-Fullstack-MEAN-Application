4af193e2ad33442f648aba7a81c1ce4e3d8040d1
arizona2014 <andy.lisac@gmail.com>
2017-11-30 08:22:34 +0100

    introducing video s

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -1,11 +1,11 @@
 import { Routes, RouterModule } from "@angular/router";
-import { MessagesComponent } from "./messages/messages.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { AUTH_ROUTES } from "./auth/auth.routes";
+import { VideosComponent } from "./videos/videos.component";
 
 const APP_ROUTES: Routes = [
-    { path: '', redirectTo: '/messages', pathMatch: 'full' },
-    { path: 'messages', component: MessagesComponent },
+    { path: '', redirectTo: '/videos', pathMatch: 'full' },
+    { path: 'videos', component: VideosComponent },
     { path: 'auth', component: AuthenticationComponent, children: AUTH_ROUTES }
 ];
 
