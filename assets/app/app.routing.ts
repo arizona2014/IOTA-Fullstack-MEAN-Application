eea43530d3480fddbc9b412e099d20b0654d83e1
arizona2014 <andy.lisac@gmail.com>
2017-12-07 08:30:21 +0100

    introducing can activate guard in routes

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -2,11 +2,24 @@ import { Routes, RouterModule } from "@angular/router";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { AUTH_ROUTES } from "./auth/auth.routes";
 import { VideosComponent } from "./videos/videos.component";
+import {CanActivateRoutes} from "./can-activate-routes";
 
 const APP_ROUTES: Routes = [
-    { path: '', redirectTo: '/videos', pathMatch: 'full' },
-    { path: 'videos', component: VideosComponent },
-    { path: 'auth', component: AuthenticationComponent, children: AUTH_ROUTES }
+    {
+        path: '',
+        redirectTo: '/videos',
+        pathMatch: 'full'
+    },
+    {
+        path: 'videos',
+        component: VideosComponent,
+        canActivate: [CanActivateRoutes]
+    },
+    {
+        path: 'auth',
+        component: AuthenticationComponent,
+        children: AUTH_ROUTES
+    }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
