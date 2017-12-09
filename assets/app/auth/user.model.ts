67113b0f672e845557825c6ac2ed2076e51502e1
arizona2014 <andy.lisac@gmail.com>
2017-12-09 08:13:05 +0100

    modified user front model

--- assets/app/auth/user.model.ts
+++ assets/app/auth/user.model.ts
@@ -2,7 +2,7 @@
  * Created by Arizona on 10/30/2017.
  */
 export class User {
-    constructor(public email:string, public password: string, public firstName?: string, public lastName?: string){
+    constructor(public email:string, public password: string, public firstName?: string, public lastName?: string, public hasPayed?: string){
 
     }
 }
