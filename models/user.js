94b6d3e028bed3e88783ae03352a356f461454f7
arizona2014 <andy.lisac@gmail.com>
2017-12-09 08:12:30 +0100

    modified user model

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ var schema = new Schema({
     lastName: {type: String, required: true },
     password: {type: String, required: true },
     email: {type: String, required: true, unique:true},
+    hasPayed: {type: String, required: true },
     videos: [{type: Schema.Types.ObjectId, ref: 'Video' }]
 });
 
