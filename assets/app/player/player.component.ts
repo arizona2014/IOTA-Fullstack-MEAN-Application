cb1a453ebcce161c4f802a1fada5bfd7863b39b7
arizona2014 <andy.lisac@gmail.com>
2017-12-09 08:11:40 +0100

    introducing player class

---
+++ assets/app/player/player.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from "@angular/core";
+import { Video } from "./video.model";
+import { VideoService } from "./video.service";
+
+@Component({
+    selector: 'app-player',
+    templateUrl: './player.component.html'
+})
+export class PlayerComponent {
+
+    constructor() {}
+
+}
