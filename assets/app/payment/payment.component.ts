d8009c6e1d3667d6c0b3104e5bf30f0cc985b470
arizona2014 <andy.lisac@gmail.com>
2017-12-13 08:16:09 +0100

    introducing payment component

---
+++ assets/app/payment/payment.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'app-payment',
+    templateUrl: './payment.component.html'
+})
+
+export class PaymentComponent {
+
+    constructor() { }
+
+
+
+}
